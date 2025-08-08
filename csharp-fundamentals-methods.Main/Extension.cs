374652e847032eca36d375cbc6ed3293b996b93c
Mona Eikli Andresen <monaeik@hotmail.com>
2025-08-08 13:24:16 +0200

    Mona Eikli

--- csharp-fundamentals-methods.Main/Extension.cs
+++ csharp-fundamentals-methods.Main/Extension.cs
@@ -20,7 +20,19 @@ namespace csharp_fundamentals_methods.Main
         */
         public string timerStatus(int timeLeftInMin)
         {
-            throw new NotImplementedException();
+            if(timeLeftInMin > 0)
+            {
+                return "The cake is still baking!";
+            }
+            else if(timeLeftInMin == 0)
+            {
+                return "The cake is ready!";
+            }
+            else 
+            {
+                return "The timer finished ages ago!";
+            }
+  
         }
 
         //TODO: Extension 2: Estimate Prep Time
@@ -35,7 +47,11 @@ namespace csharp_fundamentals_methods.Main
 
         public double estimatePrepTime(string[] ingredients, int prepTimePerIngredient)
         {
-            throw new NotImplementedException();
+            if (prepTimePerIngredient == 0) {
+                prepTimePerIngredient = 2;
+            }
+            return ingredients.Length * prepTimePerIngredient;
+
         }
 
         //TODO: Extension 3: calculateGramsOfSugar that accepts two parameters 1 an array of ingredients that will always contain 3 ingredients AND  2 the number of layers the cake has. The cake will need 100g of sugar per layer, if that ingredient is present in the provided list of ingredients.             The method should return the number of grams of sugar needed to make the cake.
@@ -49,7 +65,14 @@ namespace csharp_fundamentals_methods.Main
 
         public double calculateGramsOfSugar(string[] ingredients, int numberOfCakeLayers)
         {
-            throw new NotImplementedException();
+            if (ingredients.Contains("sugar"))
+            {
+                return numberOfCakeLayers * 100;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
